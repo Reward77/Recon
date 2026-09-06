acd8ac98658708fee2431ced51804ea27f8ffcfb
Reward77 <148389667+Reward77@users.noreply.github.com>
2026-09-06 09:36:58 +0100

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -382,4 +382,5 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allow_headers=["Authorization", "Content-Type", "X-Request-Id", "X-CSRF-Token"],
+
 )

d01a526c186abaca081038ed394f737afd2368e8
Reward77 <148389667+Reward77@users.noreply.github.com>
2026-09-06 09:11:14 +0100

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 import sys
 from pathlib import Path
 
+from fastapi.staticfiles import StaticFiles
 ROOT_DIR = Path(__file__).resolve().parent.parent
 if str(ROOT_DIR) not in sys.path:
     sys.path.insert(0, str(ROOT_DIR))
